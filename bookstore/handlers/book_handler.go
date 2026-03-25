fcfa652f84cd7d329a6dc2fb12a0c6a89b4824d8
Baisangur <baisangur.baisarov@narxoz.kz>
2026-03-25 14:49:02 +0500

    book_nandler.go file update commit

--- bookstore/handlers/book_handler.go
+++ bookstore/handlers/book_handler.go
@@ -13,20 +13,41 @@ var Books []models.Book
 var BookID = 1
 
 func GetBooks(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(Books)
-}
 
-func GetBookByID(w http.ResponseWriter, r *http.Request) {
-	idStr := strings.TrimPrefix(r.URL.Path, "/books/")
-	id, _ := strconv.Atoi(idStr)
+	pageStr := r.URL.Query().Get("page")
+	limitStr := r.URL.Query().Get("limit")
+	categoryStr := r.URL.Query().Get("category")
 
-	for _, b := range Books {
-		if b.ID == id {
-			json.NewEncoder(w).Encode(b)
-			return
-		}
+	page, _ := strconv.Atoi(pageStr)
+	limit, _ := strconv.Atoi(limitStr)
+	categoryID, _ := strconv.Atoi(categoryStr)
+
+	if page <= 0 {
+		page = 1
 	}
-	http.Error(w, "Not found", 404)
+	if limit <= 0 {
+		limit = 5
+	}
+
+	var filtered []models.Book
+	for _, b := range Books {
+		if categoryStr != "" && b.CategoryID != categoryID {
+			continue
+		}
+		filtered = append(filtered, b)
+	}
+
+	start := (page - 1) * limit
+	end := start + limit
+
+	if start > len(filtered) {
+		start = len(filtered)
+	}
+	if end > len(filtered) {
+		end = len(filtered)
+	}
+
+	json.NewEncoder(w).Encode(filtered[start:end])
 }
 
 func CreateBook(w http.ResponseWriter, r *http.Request) {
